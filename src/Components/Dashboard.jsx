c213e55d9ef3246bc4c54e67dcf8ad3cf6dbaf48
gugun_guntur <gugunguntur99@gmail.com>
2020-10-10 17:25:20 +0700

    fetch data from firebase

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -5,17 +5,38 @@ class Dashboard extends Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            products: [],
+            // setProduct: []
         }
     }
     logout() {
         fire.auth().signOut();
     }
+
+    componentDidMount() {
+        const db = fire.firestore();
+        db.collection("products")
+            .get()
+            .then(querySnapshot => {
+                const data = querySnapshot.docs.map(doc => doc.data());
+                console.log(data);
+                this.setState({ products: data });
+            });
+    }
     render() {
+
+        const {products} = this.state;
+
         return (
             <div>
                 <h1>You are logged in !!!</h1>
+
                 <button onClick={this.logout}>Logout</button>
+                <ul>
+                    {products.map(product => (
+                        <li key={product.name}>{product.name} - {product.desc}</li>
+                    ))}
+                </ul>
             </div>
         )
     }
